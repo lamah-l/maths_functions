6e51e65a1535bc85bb33d4c95e638c2f1de28d39
Lamah langlais <p14150081@email.dmu.ac.uk>
2016-02-22 19:17:57 +0000

    oppo.c uses the addition operation.

---
+++ oppo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int z, x;
+   
+	printf("%i + %i = %i\n", z, c, z + c);
+	printf("%i - %i = %i\n", z, c, z - c);
+	printf("%i x %i = %i\n", z, c, z * c);
+	printf("%i / %i = %i\n", z, c, z / c);
+    
+	return 0;
+}	
