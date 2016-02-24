a9cd7834a8f8ffedc4c959b1b53e4ddfe716930c
Lamah langlais <p14150081@email.dmu.ac.uk>
2016-02-24 01:10:34 +0000

    confilct in the master branch

--- maths.c
+++ maths.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 <<<<<<< HEAD
 //This is the line causing issue in the conflict branch
+//This is the second line causing issue in the *master branch
 =======
 
 //This is the line causing issue in the conflict branch
