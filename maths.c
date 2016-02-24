741f5bef03b86315cd8bfa916462d174bb78c008
Lamah langlais <p14150081@email.dmu.ac.uk>
2016-02-24 00:25:09 +0000

    I added modulus to the file

--- maths.c
+++ maths.c
@@ -2,12 +2,13 @@
 
 int main ()
 {
-	int x, c;
+	int x = 10, c = 5;
 
 	printf(" %2i + %2i = %2i \n", x, c, x + c );
 	printf(" %2i - %2i = %2i \n", x, c, x - c );
 	printf(" %2i x %2i = %2i \n", x, c, x * c );
 	printf(" %2i / %2i = %2i \n", x, c, x / c );
+	printf(" %2i modulus %2i = %2i \n", x, c, x % c);
 
 	return 0;
 
